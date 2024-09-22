60ceafe49605880644e20f00bfe6b7e6c38991da
Abdul Rehman Khams <111648325+ark331@users.noreply.github.com>
2024-09-22 21:14:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, request, render_template, redirect
 from flask import Request, Response, url_for, jsonify
 from pathlib import Path
 from backend.detection import predict_pill
+from backend.detection import get_drug_info
 from werkzeug.datastructures import FileStorage
 import requests
-
+from dotenv import load_dotenv
+from pprint import pprint
+import pandas as pd
+import openpyxl
 
 app = Flask(__name__, template_folder='frontend', static_folder='frontend/static')
  
@@ -23,37 +27,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/result')
-def result():
-    return render_template('re.html', content = request.args.get('content'))
-
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
-
-# @app.route('/upload', methods=['GET','POST'])
-# def upload():
-#     r: Request = request
-#     if 'file' not in r.files: 
-#         return "No file part"
-    
-#     file: FileStorage = r.files['file']
-#     print(file)
-    
-#     if file.filename == '':
-#         return "No selected file"
-    
-#     if file:
-#         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#         file.save(filename)
-
-        
-#         path =os.path.join(os.getcwd(), os.listdir('uploads')[0])
-#         if path:
-#             data = predict_pill(path)
-#             classi = data['predictions'][0]['class']
-#             print(classi)
-#             return jsonify({'class': classi})
 
 
 @app.route('/product', methods=['GET','POST'])
@@ -81,6 +57,60 @@ def upload_file():
 
 def secure_filename(filename):
     return filename
+
+@app.route('/search')
+
+def search():
+    query = request.args.get('query')
+    if query:
+        return jsonify(get_drug_info(query))
+    return jsonify({"results": []})
+
+load_dotenv()
+API_KEY = os.environ.get('ROBOFLOW_API_KEY')
+print(API_KEY)
+
+
+def get_drug_info(query):
+    url = f"https://drugapi.p.rapidapi.com/Drug/Summary/{query}"
+
+    headers = {
+    	"x-rapidapi-key": "5d53f0c48bmsh7b773967c027048p1e9882jsn9dd54bf50d87",
+    	"x-rapidapi-host": "drugapi.p.rapidapi.com"
+    }
+
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        pprint(data)
+    else:
+        print(f"Request for drug info failed with status code {response.status_code}")
+
+
+@app.route('/submit_form',methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    form_data = {
+        'Name': [name],
+        'Email': [email],
+        'Message': [message]
+    }
+
+    write_data = pd.DataFrame(form_data)
+    try:
+        with pd.ExcelWriter('contact_responses.xlsx',mode='a' ,engine='openpyxl', if_sheet_exists='overlay') as writer:
+            write_data.to_excel(writer,index=False, header=False, startrow=writer.sheets['Sheet1'].max_row)
+    except FileNotFoundError:
+        write_data.to_excel('contact_responses.xlsx', index=False)
+    except PermissionError:
+        return "File is being used or open."
+    except Exception as e:
+        return f"An unknown error occued {e}"
+    return "Thank You for Your response"
 
 if __name__ == "__main__":
 
